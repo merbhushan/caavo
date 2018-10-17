881fb6f1be0a8004a10f4c758323c7e7243903fd
Bhushan Mer <bhushanmer@gmail.com>
2018-10-18 00:29:54 +0530

    Sidebar icon

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,80 @@
 <template>
-  <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
+  <v-app
+    id="inspire"
+    dark
+  >
+    <v-navigation-drawer v-model="drawer" fixed clipped app>
+      <v-list dense>
+        <v-list-tile v-for="item in items" :key="item.text" @click="">
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <img :src="getLogo(item.icon)" alt="caavo.js" title="Caavo">
           </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
+    <v-toolbar color="grey darken-3" dense fixed clipped-left app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <img src="@/assets/icons/logo.svg" alt="caavo.js" title="Caavo">
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-layout row align-center style="max-width: 650px">
+        <v-text-field
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          single-line
+          append-icon="search"
+          color="white"
+          hide-details
+        ></v-text-field>
+      </v-layout>
     </v-toolbar>
     <v-content>
-      <router-view/>
+      <v-container fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <v-tooltip right>
+              <v-btn
+                slot="activator"
+                :href="source"
+                icon
+                large
+                target="_blank"
+              >
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/YeRKwQ" target="_blank">
+                <v-icon large>mdi-codepen</v-icon>
+              </v-btn>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  data () {
-    return {
-      clipped: false,
+  export default {
+    data: () => ({
       drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      items: [
+        { icon: 'dish', text: 'Most Popular' },
+        { icon: 'amazon', text: 'Subscriptions' },
+        { icon: 'hbogo', text: 'History' },
+        { icon: 'netflix', text: 'Playlists' },
+        { icon: 'showtime', text: 'Watch Later' }
+      ]
+    }),
+    methods: {
+      getLogo(strIcon){
+        return '@/assets/icons/' +strIcon +'.svg';
+      }
+    },
+    props: {
+      source: String
     }
   }
-}
 </script>
