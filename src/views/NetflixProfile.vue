a9cbe8af6d5ada8778e932fb7f93db82c54feaaa
Bhushan Mer <bhushanmer@gmail.com>
2018-10-21 08:34:40 +0530

    Netflix profile selection component

---
+++ src/views/NetflixProfile.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-layout row wrap>
+        <v-flex xs12 sm10 offset-sm1>
+            <v-card>
+                <v-card-title primary-title>
+                <div style="width: 100%">
+                    <div class="headline">Select Profile</div>
+                    <div >
+                        <v-layout row>
+                            <v-flex :key="profile.service" xs4 sm2 style="text-align: center;" v-for="profile in arrProfiles">
+                                <a @click="updateNetflixProfile(profile.service)">
+                                    <v-avatar color="teal">
+                                        <span class="white--text headline">{{getFirstCharcter(profile.name)}}</span>
+                                    </v-avatar>
+                                    <div style="color: white;">{{profile.name}}</div>
+                                </a>
+                            </v-flex>
+                        </v-layout>
+                    </div>
+                </div>
+                </v-card-title>
+            </v-card>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+export default {
+    props: ['arrProfiles'],
+    methods:{
+        getFirstCharcter(strName){
+            return strName.substr(0,1).toUpperCase();
+        },
+        updateNetflixProfile(strService){
+            this.$emit('update-netflix-profile', strService);
+        }
+    }
+}
+</script>
