15545cbf036c0218eae2fee525a595d330dddf36
Bhushan Mer <bhushanmer@gmail.com>
2018-10-21 08:32:59 +0530

    Vuetify Missing component added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -9,9 +9,21 @@ import {
   VIcon,
   VGrid,
   VToolbar,
+  VTooltip,
+  VTextField,
+  VCard,
+  VImg,
+  VDivider,
+  VRating,
+  VAvatar,
+  VTabs,
   transitions
 } from 'vuetify'
 import 'vuetify/src/stylus/app.styl'
+
+const MY_ICONS = {
+  'view_show': 'fas fa-eye'
+}
 
 Vue.use(Vuetify, {
   components: {
@@ -23,8 +35,17 @@ Vue.use(Vuetify, {
     VIcon,
     VGrid,
     VToolbar,
+    VTooltip,
+    VTextField,
+    VCard,
+    VImg,
+    VDivider,
+    VRating,
+    VAvatar,
+    VTabs,
     transitions
   },
   customProperties: true,
+  icons: MY_ICONS,
   iconfont: 'md',
 })
